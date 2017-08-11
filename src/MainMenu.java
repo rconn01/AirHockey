4d39c85f029a26c26777cd210ad54fccc715b528
Sam Romeo <sxr7299@g.rit.edu>
2017-08-10 20:05:44 -0400

    added a rules button to the menu so when pressed a new window opens and displays the rules

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.ImageObserver;
 
 /**
  * Created on 8/5/17
@@ -10,16 +11,15 @@ public class MainMenu {
     JFrame Menu = new JFrame("AirHockey");
     JButton Single = new JButton("Single");
     JButton Double = new JButton("Double");
-    JButton Exit = new JButton("Exit");
-    JLabel label = new JLabel("Air Hockey", SwingConstants.CENTER);
-    JTextArea rules = new JTextArea("Rules: " + "\nFor doubles try and be the first to 7 points"  + "\nFor single player, try and get the highest amount of " + "\npoints without the ball hitting the bottom.");
+    JButton Rules = new JButton("Rules");
+    JLabel label = new JLabel("Air Hockey", JLabel.CENTER);
 
-    int menuWidth = 166;//size of each button
-    int menuHeight = 60;//width of each button
-    int menuY = 189;//y value of where button will be placed
+    int menuWidth = 333;//width of each button
+    int menuHeight = 120;//height of each button
+    int menuY = 430;//y value of where button will be placed
     /*Dimensions for the frame*/
-    int Width = 500;
-    int Height = 300;
+    int Width = 1000;
+    int Height = 600;
 
     /**
      * Creates the main menu with 2 buttons and displays main rules
@@ -29,13 +29,13 @@ public class MainMenu {
         Menu.setResizable(false);
         Menu.setSize(Width, Height);
         //Menu.setLayout(null);
-        Menu.setLocationRelativeTo(null);
+        //Menu.setLocationRelativeTo(null);
         Menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Single Button
         Single.setSize(menuWidth, menuHeight);
         Single.setLocation(0,menuY);
-        Single.setFont(new Font("Arial", Font.BOLD, 25));
+        Single.setFont(new Font("Stencil", Font.PLAIN, 45));
         Menu.add(Single);
         Single.addActionListener(e -> {
                     new Thread(() -> new AirHockeyGame('S').start()).start();
@@ -44,8 +44,8 @@ public class MainMenu {
 
         //Double Button
         Double.setSize(menuWidth, menuHeight);
-        Double.setLocation(171, menuY);
-        Double.setFont(new Font("Arial", Font.BOLD, 25));
+        Double.setLocation(338, menuY);
+        Double.setFont(new Font("Stencil", Font.PLAIN, 45));
         //Double.setBackground(Color.RED);
         Menu.add(Double);
         Double.addActionListener(e -> {
@@ -53,24 +53,32 @@ public class MainMenu {
             Menu.setVisible(false);
         });
 
-        //Exit Button Variables
-        Exit.setSize(menuWidth, menuHeight);
-        Exit.setLocation(342,menuY);
-        Exit.setFont(new Font("Arial", Font.BOLD, 25));
+        //Rules Button Variables
+        Rules.setSize(menuWidth, menuHeight);
+        Rules.setLocation(676,menuY);
+        Rules.setFont(new Font("Stencil", Font.PLAIN, 45));
         //Exit.setBackground(Color.BLUE);
-        Menu.add(Exit);
-        Exit.addActionListener(e -> System.exit(0));
+        Menu.add(Rules);
+        Rules.addActionListener(e -> {
+            JFrame rulesMenu = new JFrame("Rules");
+            JButton ok = new JButton("OK");
+            rulesMenu.setSize(Width, Height);
+            JTextArea text = new JTextArea("Double: Try to be the first to 7 points by having the puck hit\n              the other players side. \n\nSingle: Try score as many points by preventing the puck from\n             hitting the bottom.");
+            text.setFont(new Font("Arial", Font.PLAIN, 30));
+            ok.setFont(new Font("Stencil", Font.PLAIN, 45));
+            ok.setSize(menuWidth, menuHeight);
+            ok.setLocation(330, 380);
+            ok.addActionListener(a -> rulesMenu.setVisible(false));
+            rulesMenu.add(ok);
+            rulesMenu.add(text);
+            rulesMenu.setVisible(true);
+        });
 
         //Display label
-        //label.setSize(menuWidth, menuHeight);
-        //label.setLocation(-250, 0);
-        label.setFont(new Font("Stencil", Font.ITALIC, 30));
+        label.setSize(menuWidth, menuHeight);
+        label.setLocation(5,5);
+        label.setFont(new Font("Stencil", Font.ITALIC, 150));
         Menu.add(label);
-
-        //Rules Label
-        rules.setLocation(250, 150);
-        rules.setFont(new Font("Arial", Font.PLAIN, 20));
-        Menu.add(rules);
 
         Menu.setVisible(true);
 
